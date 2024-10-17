d1635d36465a1337ba3384aab113cc062af8ea34
Pedrohp13 <137409999+Pedrohp13@users.noreply.github.com>
2024-10-17 09:59:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Realm dos Guri',
+    'O que é o mine dos guri?',
+    ' é uma jogatina insana com os guri do 2 ano B'
+)
+
+criaCartao(
+    'Marcos Henrique fillus',
+    'Por que o Marcos gosta tanto de maionese?',
+    'Segundo o mesmo: "Eu não gosto, eu odeio maionese"'
+)
+
+criaCartao(
+    'Musica',
+    'Por que Tempo Perdido do Legião Urbana é tão bom?',
+    'Pois o Renato Russo manjava dms slk, tempo perdido é obra prima'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que é Poggers?',
+    'É uma palavra que representa uma surpresa, tipo "aquilo foi muito poggers"'
+)
